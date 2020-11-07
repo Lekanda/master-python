7172ce267982e27208d8e70691f7209952fefdcd
lekanda <abernaolao@gmail.com>
2020-11-07 14:57:45 +0100

    Hola Mundo y comentarios en Python

---
+++ 01-holamundo/comentarios.py
@@ -0,0 +1,15 @@
+# Esto me muestra el texto en consola o pantalla
+
+
+print("Hola")
+print("Mundo")
+print("!!")
+
+"""
+Esto no se muestra por las comillas
+print("Hola")
+print("Mundo")
+print("!!")
+"""
+
+print("!!")
