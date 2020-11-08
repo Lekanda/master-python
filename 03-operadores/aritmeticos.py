b719d8a1793f7658ea73558aebe719fc3678e8a0
lekanda <abernaolao@gmail.com>
2020-11-08 11:34:04 +0100

    Operadores

---
+++ 03-operadores/aritmeticos.py
@@ -0,0 +1,11 @@
+# Operadores Aritmeticos
+
+num1 = 78
+num2 = 2 # Operador de asignacion
+
+resta = num1 - num2 # Igual con + , * , /
+print(resta)
+
+print(f"La  suma es {num1+num2}")
+print(f"El RESTO es {num1 % num2}")
+
---
+++ 03-operadores/asignacion.py
@@ -0,0 +1,27 @@
+#Operadores de Asignacion
+edad = 55 # Da 55
+print(edad)
+
+edad += 5 # Da 60, igual con - * /
+edad=edad+5 # Lo mismo
+print(edad)
+
+# Operadores de incremento y decremento
+year = 2021
+"""
+++year  # Esto no funciona asi en Py
+year++
+"""
+# Incremento, igual con - * /
+year=year+1
+year += 1
+
+# pre-incremento, igual con - * /
+year=1+year
+
+# pre-decremento
+year=1-year
+
+
+
+print(year)
