fe7d5382af7e8c1b1238cf3f38f9240fb41cd2f5
lekanda <abernaolao@gmail.com>
2020-11-11 11:34:43 +0100

    38.Crear Funciones

---
+++ 08-funciones/main.py
@@ -0,0 +1,25 @@
+"""
+Funcion: Un conjunto de instrucciones agrupadas en un nombre
+
+def nombreFuncion(parametros):
+    # Bloque de codigo 
+
+
+# Para invocar a la funcion
+nombreFuncion(parametro/s)
+
+"""
+
+# Ejemplo 1
+nombre=input("dame un nombre: ")
+print("\n")
+print("########## EJEMPLO1 ##########")
+print("\n\n")
+
+def muestraNombres(nombre):
+    print("Andres")
+    print("Bernaola")
+    print(f"hola {nombre}")
+    print("\n\n")
+
+muestraNombres(nombre)
