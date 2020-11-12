0bc874dee07f8ba795ea55aef152d0d011729be7
lekanda <abernaolao@gmail.com>
2020-11-12 11:36:23 +0100

    45-Variables Locales y Globales

---
+++ 08-funciones/variables.py
@@ -0,0 +1,12 @@
+"""
+DIFERENCIAS ENTRE VARIABLE LOCAL Y GLOBAL:
+-LOCAL: Es la usada dentro de la funcion y se puede hacer return
+-GLOBAL: Estan fuera de la funcion y estan disponibles para todas las funciones
+
+- Para hacer que una variable local sea global se pone antes de crearla :
+                 global num
+                 num = 12
+
+
+"""
+
