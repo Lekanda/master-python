46a6b6f28ce17b63f68dd5cea9dfd553a41dd3e4
lekanda <abernaolao@gmail.com>
2020-11-13 10:37:55 +0100

    49-Indice de Listas

--- 09-listas/main.py
+++ 09-listas/main.py
@@ -9,13 +9,24 @@
 # Definir una lista
 peliculas = ["Batman", "Spiderman", "el señor de los anillos", "terminator"]
   # Con tupla. Entre (). En 
-cantantes = list(('Van Morrison', 'Yes', 'David Bowie'))
+cantantes = list(('Van Morrison', 'Yes', 'David Bowie', 'Neil Young'))
   # Linea 14 : Crea una lista desde el 2020 a 2050
 years=list(range(2020,2050))
   # Se puede meter en un lista datos de diferentes tipos
 variada= ["Andres", 30, 4.5, True, "texto"]
 
+# print(peliculas)
+# print(cantantes)
+# print(years)
+peliculas[1]="Gran Torino"
 
-print(peliculas)
-print(cantantes)
-print(years)
+print(peliculas[0])
+    # li-25.Empieza desde el final de la lista
+print(peliculas[-2])
+    # li-27 Saca del 1 al 3
+print(cantantes[1:3])
+    # li-29: Todos a partir del 0
+print(cantantes[0:])
+
+
+
