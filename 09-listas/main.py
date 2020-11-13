635c5b2c0f6415abd81eeb321262dd01538c0f0d
lekanda <abernaolao@gmail.com>
2020-11-13 10:26:17 +0100

    48-Crear listas

---
+++ 09-listas/main.py
@@ -0,0 +1,21 @@
+"""
+                 **LISTAS (arrays)**
+-Son colecciones de Datos/valores, bajo un unico nombre.
+-Para acceder a esos valores usamos un indice numerico
+
+- TUPLA: Es lo mismo que una lista pero sus valores no pueden cambiar. Para crearla con LIST ponerlo entre parentesis. Ver ejemplo abajo
+"""
+
+# Definir una lista
+peliculas = ["Batman", "Spiderman", "el señor de los anillos", "terminator"]
+  # Con tupla. Entre (). En 
+cantantes = list(('Van Morrison', 'Yes', 'David Bowie'))
+  # Linea 14 : Crea una lista desde el 2020 a 2050
+years=list(range(2020,2050))
+  # Se puede meter en un lista datos de diferentes tipos
+variada= ["Andres", 30, 4.5, True, "texto"]
+
+
+print(peliculas)
+print(cantantes)
+print(years)
