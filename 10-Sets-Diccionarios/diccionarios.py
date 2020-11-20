f99aa01975209a2bbf61a2eda3d0fdca91663076
lekanda <abernaolao@gmail.com>
2020-11-20 22:26:53 +0100

    55-Diccionarios

---
+++ 10-Sets-Diccionarios/diccionarios.py
@@ -0,0 +1,17 @@
+"""
+***************** Diccionarios *******************
+--------------------------------------------------
+- Es un tipo de dato que almacena un conjunto de datos en formato clave-valor y es parecido a un array asociativo o un objeto JSON. Son como las listas pero en vez de tener indice numerico, lo tiene alfanumerico
+
+"""
+
+persona = {
+    "nombre": " Andres",
+    "apellidos": "Bernaola",
+    "web": "www.andresbernaola.com"
+}
+
+print(persona)
+print(type(persona))
+print(persona["apellidos"])
+print(persona["web"])
