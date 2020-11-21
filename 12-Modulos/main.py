a1048fdec07dac1cf0c2de56676a551bab685f25
lekanda <abernaolao@gmail.com>
2020-11-21 16:42:56 +0100

    borrar carpeta clases

---
+++ 12-Modulos/main.py
@@ -0,0 +1,10 @@
+"""
+- Definicion: Los modulos son funcionalidadesya hechaspara reutilizar
+- Hay muchos modulos hechos ya: (Python Module Index)
+
+        https://docs.python.org/3/py-modindex.html
+
+
+
+"""
+
