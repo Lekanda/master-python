140910b9d268d4851fcdb7bd1c2a3b83cef9587c
lekanda <abernaolao@gmail.com>
2020-11-22 09:48:49 +0100

    70-Crear Ficheros

--- 14-Sistema-Archivos/ficheros.py
+++ 14-Sistema-Archivos/ficheros.py
@@ -3,7 +3,6 @@ import pathlib
 
 
 
-
 # Abrir archivo
 ruta=str(pathlib.Path().absolute()) + "/fichero_texto.txt"
 print(ruta)
@@ -14,5 +13,29 @@ archivo = open(ruta, "a+")
 archivo.write("*****HOLA desde python********\n")
 
 
-# CLOSE Crerra el archivo. ¡¡SIEMPRE!! hay que cerrar los archivos
+# CLOSE Cierra el archivo. 
+#           "¡¡SIEMPRE!! hay que cerrar los archivos"
 archivo.close()
+
+
+# Abrir archivo para LEER
+ruta=str(pathlib.Path().absolute()) + "/fichero_texto.txt"
+print(ruta)
+archivo_lectura = open(ruta, "r")
+# Leer contenido del archivo abierto
+# contenido= archivo_lectura.read()
+# print(contenido)# Lee por lineas
+# for elemento in contenido: # Lee con caracter
+#     print(elemento)
+
+# Leer contenido y guardar en lista
+lista = archivo_lectura.readlines()
+archivo_lectura.close()
+# print(lista)
+for elemento in lista: # Lee con caracter
+    print(elemento)
+
+for frase in lista:
+    lista_frase = frase.split() # Crea una lista con cada elemento
+    print(lista_frase)
+    # print("-"+frase.capitalize())
