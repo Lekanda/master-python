c4b9b1deb66a60c03ded0f047ca3d6ffce186b6d
lekanda <abernaolao@gmail.com>
2020-11-22 14:14:24 +0100

    77- Multiples errores

--- 15-manejo de errores/main.py
+++ 15-manejo de errores/main.py
@@ -3,11 +3,15 @@
 """
                 TRY,EXCEPT,ELSE(op) Y FINALLY(op)
                 ---------------------------------
-- Esta es la forma que tiene python de hacer un try/catch de JavScript.
+- Seria en  python como hacer un try/catch similar al de JavScript.
 - El else y el finally son opcionales:
     -ELSE=> Lo hace si ha salido bien
-    -FINALLY => Lo hace siempre. Haya ssalido bien o mal
-- TRY Y EXCEPT => Como try(ok) catch(nok)
+    -FINALLY => Lo hace siempre. Haya salido bien o mal
+- TRY Y EXCEPT => Como 'try(ok) / catch(nok)'
+"""
+
+
+
 """
 try:
     nombre=input("Cual es tu nombre?: ")
@@ -22,3 +26,34 @@ else: # Opcional
     print("Todo ha ido bien!!")
 finally:
     print("Fin de la iteracion")
+"""
+
+
+
+
+# MULTIPLES excepciones
+
+try:
+    numero=int(input("Numero para elevarlo al cuadrado: "))
+    print("El cuadrado del numero es: " + str(numero*numero))
+    
+# Error de codigo    
+except TypeError:
+    print("Debes convertir tus cadenas a entero en el codigo")
+
+# Error de letras y vacio
+# except ValueError:
+#     print("Ni vacio ni letras, introduce un numero")
+
+# error personalizado. No va si typeError esta puesto
+except Exception as e:
+    print(type(e))
+    print("Ha ocurrido un error: ", type(e).__name__)
+
+
+
+
+
+
+
+
