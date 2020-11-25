5ec96ec911e9dd40db1abe4ece1f2931169a0c8b
lekanda <abernaolao@gmail.com>
2020-11-25 10:48:54 +0100

    100- SELECT Y WHERE

--- 19-bases-datos/main.py
+++ 19-bases-datos/main.py
@@ -1,6 +1,7 @@
 import mysql.connector
-
+##################
 # Conexion a la DB
+##################
 database = mysql.connector.connect(
     host ="localhost",
     user = "root",
@@ -17,7 +18,10 @@ database = mysql.connector.connect(
 cursor = database.cursor()
 
 
-# Crear DB 
+
+##########
+# Crear DB
+# ######## 
 cursor.execute("CREATE DATABASE IF NOT EXISTS master_python")
 """
 # Comprobar que se ha creado la DB
@@ -25,7 +29,12 @@ cursor.execute("SHOW DATABASES")
 # for db in cursor: # Itera en la  DB, las abiertas en MySQL
 #     print(db)
 """
+
+
+
+##############
 # Crear Tablas
+##############
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS vehiculos(
 id int(10) auto_increment not null,
@@ -36,13 +45,16 @@ CONSTRAINT pk_vehiculo PRIMARY KEY(id)
 )
 """)
 
-cursor.execute("SHOW TABLES")
-for table in cursor:
-    print(table) # Nos da las tablas creadas en la DB creada
+# cursor.execute("SHOW TABLES")
+# for table in cursor:
+#     print(table) # Nos da las tablas creadas en la DB creada
 
 
 
+# 
+################################################################
 # Insertar datos en la DB 'vehiculos' y en la tabla 'vehiculo'
+################################################################
 # cursor.execute("INSERT INTO vehiculos VALUES(null,'Opel','Corsa',1000)") # Inserta un registro
 coches = [
     ('Opel','Astra',1000),
@@ -53,3 +65,36 @@ coches = [
 ]
 # cursor.executemany("INSERT INTO vehiculos VALUES(null,%s,%s,%s)", coches) # Inserta muchos registros a la vez
 database.commit() # Guarda en la DB
+
+
+########################
+# Leer datos desde la DB
+########################
+# Modo 1
+"""
+cursor.execute("SELECT * FROM vehiculos")
+# cursor.execute("SELECT marca, precio FROM vehiculos") # Trae esos 2 solo
+result = cursor.fetchall()
+
+for coche in result:
+    # print(coche)
+    # print(coche[0])
+    print(coche[1], coche[3])
+"""
+
+# Modo 2 ( Con mas condiciones de busqueda que el Modo 1)
+cursor.execute("SELECT * FROM vehiculos WHERE precio>9000 AND marca='BMW'")
+# cursor.execute("SELECT marca, precio FROM vehiculos") # Trae esos 2 solo
+result = cursor.fetchall()
+
+for coche in result:
+    # print(coche)
+    # print(coche[0])
+    print(coche[1], coche[3])
+
+
+# Modo 3 ( Con 'FetchOne' traemos solo el primer registro de la DB)
+cursor.execute("SELECT * FROM vehiculos")
+# cursor.execute("SELECT marca, precio FROM vehiculos") # Trae esos 2 solo
+coche = cursor.fetchone()
+print(coche)
