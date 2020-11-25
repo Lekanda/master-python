8d74b8d2535c8af6889f85bd5e0121990a68825c
lekanda <abernaolao@gmail.com>
2020-11-25 11:48:41 +0100

    103- Primeros pasos del proyecto notas-consola

---
+++ 20-proyecto-python/main.py
@@ -0,0 +1,17 @@
+"""
+            APLICACION DE NOTAS EN CONSOLA
+            ------------------------------
+- abrir asistente que pregunte sí queremos hacer:
+        - login
+        - Registro
+- Sí Registro = Crear Cuenta de Uss en la DB
+- Sí login = Loguerse en la DB y menu de acciones desde la DB:
+    - Crear Nota
+    - Leer Nota
+    - Borrar Nota
+"""
+
+
+
+
+
