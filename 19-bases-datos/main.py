5950852224935b30f2cc9c40f5b81f1a0ef870f5
lekanda <abernaolao@gmail.com>
2020-11-25 10:11:36 +0100

    98- Crear Tablas en MySQL

--- 19-bases-datos/main.py
+++ 19-bases-datos/main.py
@@ -12,12 +12,31 @@ database = mysql.connector.connect(
 # print(database) 
 # Bebe dar algo asi: <mysql.connector.connection.MySQLConnection object at 0x000001F89EE9E970>
 
+
+# Cursor: Nos permite ejecutar las consultas
 cursor = database.cursor()
 
+
+# Crear DB 
 cursor.execute("CREATE DATABASE IF NOT EXISTS master_python")
-
-    # Comprobar que se ha creado la DB
+"""
+# Comprobar que se ha creado la DB
 cursor.execute("SHOW DATABASES")
+# for db in cursor:
+#     print(db)
+"""
+# Crear Tablas
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS vehiculos(
+id int(10) auto_increment not null,
+marca varchar(40) not null,
+modelo varchar(40) not null,
+precio float(10,2) not null,
+CONSTRAINT pk_vehiculo PRIMARY KEY(id)
+)
+""")
 
-for db in cursor:
-    print(db)
+cursor.execute("SHOW TABLES")
+
+for table in cursor:
+    print(table) # Nos da las tablas creadas en la DB creada
