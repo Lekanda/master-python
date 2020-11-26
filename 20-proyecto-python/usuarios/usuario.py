864dad3195a6bc0f180270abd52f4c78f860214d
lekanda <abernaolao@gmail.com>
2020-11-26 09:44:26 +0100

    109- Conexion a la DB MySQL

--- 20-proyecto-python/usuarios/usuario.py
+++ 20-proyecto-python/usuarios/usuario.py
@@ -1,3 +1,32 @@
+import mysql.connector
+
+##################
+# Conexion a la DB
+##################
+database = mysql.connector.connect(
+    host ="localhost",
+    user = "root",
+    passwd = "nuvN52772of5VzNL",
+    database = "master_python",
+    port = 3306
+)
+#¿La conexion ha sido correcta?
+# print(database) 
+
+
+
+#--------------------------------------------
+# Cursor: Nos permite ejecutar las consultas
+cursor = database.cursor(buffered=True)
+# buffered=True => Nos permite hacer muchas consultas utilizando el mismo cursor
+#--------------------------------------------
+
+
+
+
+
+
+
 class Usuario:
     def __init__(self, nombre, apellidos, email, password):
         self.nombre = nombre
