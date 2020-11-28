2b69afa9e06b6cce9d92a0bb5cd0eee3f3d7de2e
lekanda <abernaolao@gmail.com>
2020-11-28 15:17:28 +0100

    122- Cargar imagen de Logo correctamente

--- 21-tkinter/01-ventana.py
+++ 21-tkinter/01-ventana.py
@@ -3,12 +3,29 @@ TKINTER => Modulo para crear interfaces graficas de usuario
 """
 # Importamos todo de
 from tkinter import *
+import os.path
 
  # ********Crear la ventana raiz********
 ventana = Tk()
 
 #Titulo de la Ventana
 ventana.title("Interfaz grafica con PYTHON y TKINTER")
+
+# Comprobar si existe un archivo
+ruta_icono = os.path.abspath('./imagenes/Star.ico')
+# print(f"Ruta icono : {ruta_icono}")
+
+# 
+if not os.path.isfile(ruta_icono):
+    ruta_icono = os.path.abspath('./21-tkinter/imagenes/Star.ico')
+
+# Icono de la ventana
+ventana.iconbitmap(ruta_icono)
+
+# Mostrar texto en el programa
+texto = Label(ventana, text=ruta_icono)
+texto.pack()
+
 
 
 # Cambiar el tamaño de la ventana
@@ -19,14 +36,6 @@ ventana.resizable(0,0)
     # (1,1) deja cambiar el tamaño
     # (1,0) deja cambiar el tamaño de ancho pero no del alto
     # (0,1) deja cambiar el tamaño de alto pero no del ancho
-
-# Icono de la ventana
-ventana.iconbitmap("./imagenes/Star.ico")
-
-    
-
-
-
 
 
 
