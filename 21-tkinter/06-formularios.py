194fb549ba50253ca405791a24c13ad51e611825
lekanda <abernaolao@gmail.com>
2020-11-29 14:41:49 +0100

    135- Campo Apellidos.Configuracion de campo_texto

--- 21-tkinter/06-formularios.py
+++ 21-tkinter/06-formularios.py
@@ -15,17 +15,26 @@ encabezado.config(
     padx=10,
     pady=10
 )
-encabezado.grid(row=0, column=0, columnspan=2, sticky=W)
+encabezado.grid(row=0, column=0, columnspan=12, sticky=W)
 
 
-
+#********** CAMPO NOMBRE ****************
 #Label para el campo
 label = Label(ventana, text="Nombre")
-label.grid(row=1,column=0,padx=5,pady=5, sticky=W)
+label.grid(row=1,column=0,sticky=W, padx=5,pady=5)
 # Campo de texto
 campo_texto = Entry(ventana)
 campo_texto.grid(row=1, column=1, sticky=W, padx=5,pady=5)
+campo_texto.config(justify="right", state="normal")
 
+#********** CAMPO APELLIDOS ****************
+#Label para el campo
+label = Label(ventana, text="Apellidos")
+label.grid(row=2,column=0, sticky=W, padx=5,pady=5)
+# Campo de texto
+campo_texto = Entry(ventana)
+campo_texto.grid(row=2, column=1, sticky=W, padx=5,pady=5)
+campo_texto.config(justify="right", state="disabled")
 
 
 
