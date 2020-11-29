4f5d7d9b41a51fb16abfe606cde8607d3a79231e
lekanda <abernaolao@gmail.com>
2020-11-29 08:37:53 +0100

    127.1- Funcion para pasar parametros(keyword argumentts)

--- 21-tkinter/02-textos.py
+++ 21-tkinter/02-textos.py
@@ -6,21 +6,25 @@ ventana.geometry("700x500")
 
 texto = Label(ventana, text = "Bienvenido a mi programa")
 texto.config(
-    fg="red", # Color Letra
+    fg="green", # Color Letra
     # bg="Black", # Color fondo del texto
     bg="#122523", # Color fondo del texto
     padx=50, # Margen en el eje X
     pady=20, # Margen en el eje Y
     font=("Consolas", 30) # Tamaño de la funte de texto
 )
-texto.pack()
+texto.pack(anchor=E)
 
-texto = Label(ventana, text = "Hola soy el programador")
+def pruebas(nombre,apellidos,pais):
+    return f"Hola {nombre} {apellidos} eres de {pais}"
+
+texto = Label(ventana, text = pruebas("Andres", "Bernaola", "Espña"))
+
 texto.config(
     # justify=RIGHT, # Justifica , pero solo sobre el texto, no se nota
     # width=600,
     height=3,
-    bg="orange",
+    bg="green",
     font=("Arial", 18),
     padx=10, # Margen en el eje X
     pady=10, # Margen en el eje Y
