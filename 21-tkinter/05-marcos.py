686c06bf6c8be3f913040a3b6ffc695900f7d464
lekanda <abernaolao@gmail.com>
2020-11-29 12:22:04 +0100

    131- Texto dentro de Frames

--- 21-tkinter/05-marcos.py
+++ 21-tkinter/05-marcos.py
@@ -7,17 +7,10 @@ ventana.geometry("700x700")
 
 #******************* MARCO Padre 1 ********************
 marco_padre = Frame(ventana, width=250, height=250)
-marco_padre.config(
-    bg="CornflowerBlue",
-    bd=2,
-    relief=SOLID
-)
-#relief => tipo de borde: flat, groove, raised, ridge, solid, sunken. Tambien vale 'relief=SUNKEN'
 marco_padre.pack(side=BOTTOM, fill=X, expand=YES)
 # Se puede jugar con 'anchor' y 'side'
 
-
-#******************* MARCO 1 ********************
+#********* MARCO 1  de MARCO Padre 1 ***********
 marco = Frame(marco_padre, width=250, height=250)
 marco.config(
     bg="red",
@@ -25,10 +18,13 @@ marco.config(
     relief="sunken"
 )
 #relief => tipo de borde: flat, groove, raised, ridge, solid, sunken. Tambien vale 'relief=SUNKEN'
-marco.pack(side=LEFT, anchor=SW)
+marco.pack(side=LEFT, anchor=W)
 # Se puede jugar con 'side' y 'anchor'; Con side le das la posicion y con anchor la ajustas
+Label(marco, text="Primer marco de prueba").pack(side= LEFT ,anchor=CENTER) # Introducir texto en 'ventana' y ubicarlo en ella
+marco.pack_propagate(False) # NO cambia el tamaño de la ventana
 
-#******************* MARCO 2 ********************
+
+#********** MARCO 2  de MARCO Padre 1 ***********
 marco = Frame(marco_padre, width=250, height=250)
 marco.config(
     bg="Crimson",
@@ -36,29 +32,16 @@ marco.config(
     relief=SOLID
 )
 #relief => tipo de borde: flat, groove, raised, ridge, solid, sunken. Tambien vale 'relief=SUNKEN'
-marco.pack(side=LEFT, anchor=S)
+marco.pack(side=RIGHT, anchor=S)
 # Se puede jugar con 'anchor' y 'side'
 
-#******************* MARCO 3 ********************
-marco = Frame(marco_padre, width=250, height=250)
-marco.config(
-    bg="Coral",
-    bd=2,
-    relief=SOLID
-)
-#relief => tipo de borde: flat, groove, raised, ridge, solid, sunken. Tambien vale 'relief=SUNKEN'
-marco.pack(side=LEFT, anchor=S)
-# Se puede jugar con 'anchor' y 'side'
 
+##############################################################
 
 
 #******************* MARCO Padre 2 ********************
 marco_padre = Frame(ventana, width=250, height=250)
-marco_padre.config(
-    bg="Cyan",
-    bd=2,
-    relief=SOLID
-)
+
 #relief => tipo de borde: flat, groove, raised, ridge, solid, sunken. Tambien vale 'relief=SUNKEN'
 marco_padre.pack(side=TOP, fill=X, expand=YES)
 # Se puede jugar con 'anchor' y 'side'
@@ -74,6 +57,20 @@ marco.config(
 #relief => tipo de borde: flat, groove, raised, ridge, solid, sunken. Tambien vale 'relief=SUNKEN'
 marco.pack(side=LEFT, anchor=W)
 # Se puede jugar con 'anchor' y 'side'
+
+#*********** MARCO 1 de MARCO Padre 2 ***********
+marco = Frame(marco_padre, width=250, height=250)
+marco.config(
+    bg="Coral",
+    bd=2,
+    relief=SOLID
+)
+#relief => tipo de borde: flat, groove, raised, ridge, solid, sunken. Tambien vale 'relief=SUNKEN'
+marco.pack(side=RIGHT, anchor=E)
+# Se puede jugar con 'anchor' y 'side'
+
+
+##############################################################
 
 
 
