2946d3e7282c97bb8ae8f26e7508f181e89c5d20
lekanda <abernaolao@gmail.com>
2020-12-01 18:57:00 +0100

    148-Checkbox

--- 21-tkinter/11-formularios2.py
+++ 21-tkinter/11-formularios2.py
@@ -14,17 +14,54 @@ encabezado.config(
     bg="green",
     font=("Consolas",20)
 )
-encabezado.pack(anchor=N, side=TOP, fill=X, expand=YES)
+encabezado.grid(row=0,column=0,columnspan=5,sticky=W)
+
+def mostrarProfesion():
+    texto=""
+    if web.get(): # Sí me da True
+        texto+="Desarrollo Web"
+
+    if movil.get(): # Sí me da True
+        if web.get(): # Sí me da True
+            texto+=" y Desarrollo Movil"
+        else:
+            texto+="Desarrollo Movil"
+
+
+    mostrar.config(
+        text=texto,
+        bg="green",
+        fg="white"
+    )
 
 # Botones de Check
+web= IntVar()
+movil=IntVar()
+Label(ventana, text="¿A que te dedicas?").grid(row=1, column=0,sticky=W)
+Checkbutton(
+    ventana, 
+    text="Desarrollo Web",
+    variable=web,
+    onvalue=1,
+    offvalue=0,
+    command=mostrarProfesion
+).grid(row=2, column=0,sticky=W)
+Checkbutton(
+    ventana, 
+    text="Desarrollo Movil",
+    variable=movil,
+    onvalue=1,
+    offvalue=0,
+    command=mostrarProfesion
+).grid(row=3, column=0,sticky=W)
 
+mostrar = Label(ventana)
+mostrar.grid(row=4, column=0)
 
 # Radio Buttons
 
 
 # Option Menu
-
-
 
 
 
