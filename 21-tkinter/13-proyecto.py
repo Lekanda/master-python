37283a0ff2242db7b3f270039eac166428fa4e2a
lekanda <abernaolao@gmail.com>
2020-12-02 17:38:31 +0100

    152-Enunciado Proyecto

---
+++ 21-tkinter/13-proyecto.py
@@ -0,0 +1,14 @@
+"""
+                PROYECTO
+                --------
+- crear un programa que tenga:
+    - Una ventana
+    - Tamaño fijo
+    - Ventana no redimensionable
+    -  Un Menu
+    - Diferentes pantallas
+    - Formulario de añadir productos
+    - guardar datos temporalmente
+    - MOstrar datos listados en la pantalla 'home'   
+    - Opcion de 'Salir'            
+"""
