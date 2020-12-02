9b0708af71cae0778a9f148f7cedb16d68b022d4
lekanda <abernaolao@gmail.com>
2020-12-02 17:42:30 +0100

    153-Ventana, titulo y geometria

--- 21-tkinter/13-proyecto.py
+++ 21-tkinter/13-proyecto.py
@@ -2,9 +2,9 @@
                 PROYECTO
                 --------
 - crear un programa que tenga:
-    - Una ventana
-    - Tamaño fijo
-    - Ventana no redimensionable
+    - Una ventana x
+    - Tamaño fijo x
+    - Ventana no redimensionable x
     -  Un Menu
     - Diferentes pantallas
     - Formulario de añadir productos
@@ -12,3 +12,13 @@
     - MOstrar datos listados en la pantalla 'home'   
     - Opcion de 'Salir'            
 """
+from tkinter import *
+
+ventana=Tk()
+ventana.geometry("500x500")
+ventana.title("Proyecto con TKINTER")
+ventana.resizable(0,0)
+
+
+
+ventana.mainloop()
