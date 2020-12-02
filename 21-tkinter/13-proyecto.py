34d88e20a93a246f782ddd2fddc65cccabbdf3c6
lekanda <abernaolao@gmail.com>
2020-12-02 17:52:59 +0100

    154-Menus del Programa

--- 21-tkinter/13-proyecto.py
+++ 21-tkinter/13-proyecto.py
@@ -2,23 +2,35 @@
                 PROYECTO
                 --------
 - crear un programa que tenga:
-    - Una ventana x
-    - Tamaño fijo x
-    - Ventana no redimensionable x
-    -  Un Menu
+    - Una ventana (X)
+    - Tamaño fijo (X)
+    - Ventana no redimensionable (X)
+    - Un Menu superior(Inicio,Añadir, Informacion, Salir) (X)
     - Diferentes pantallas
     - Formulario de añadir productos
     - guardar datos temporalmente
     - MOstrar datos listados en la pantalla 'home'   
-    - Opcion de 'Salir'            
+    - Opcion de 'Salir' (X)       
 """
 from tkinter import *
-
+# definir ventana
 ventana=Tk()
 ventana.geometry("500x500")
 ventana.title("Proyecto con TKINTER")
 ventana.resizable(0,0)
 
 
+# Menu Superior
+menu_superior=Menu(ventana)
+menu_superior.add_command(label="Inicio")
+menu_superior.add_command(label="Añadir")
+menu_superior.add_command(label="Informacion")
+menu_superior.add_command(label="Salir", command=ventana.quit)
+# Cargar el menu superior
+ventana.config(menu=menu_superior)
 
+
+
+
+#Cargar ventana
 ventana.mainloop()
