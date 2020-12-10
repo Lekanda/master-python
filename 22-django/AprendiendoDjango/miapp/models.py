a27fb0a727c4745971ff51944b427f4b6ff0f783
lekanda <abernaolao@gmail.com>
2020-12-10 09:57:28 +0100

    215-Crear Tablas basadas en Modelos

--- 22-django/AprendiendoDjango/miapp/models.py
+++ 22-django/AprendiendoDjango/miapp/models.py
@@ -9,7 +9,7 @@ class Article(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-class Category(model.Model):
+class Category(models.Model):
     name = models.CharField(max_length=100)
     description= models.CharField(max_length=250)
     created_at= models.DateField()
