647621b22c3c6bb756b5692be83924f990f6489f
lekanda <abernaolao@gmail.com>
2020-12-13 14:02:50 +0100

    233-Formularios basados en clases. Up en NAV de layout.html. Ajustes en CSS Texarea

---
+++ 22-django/AprendiendoDjango/miapp/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+
+class FormArticle(forms.Form):
+    title = forms.CharField(
+        label="Titulo"
+    )
+    content = forms.CharField(
+        label="Contenido",
+        widget=forms.Textarea
+    )
--- 22-django/AprendiendoDjango/miapp/views.py
+++ 22-django/AprendiendoDjango/miapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponse, redirect
 from miapp.models import Article
 from django.db.models import Q
+from miapp.forms import FormArticle
 
 
 # Create your views here.
@@ -107,6 +108,11 @@ def save_article(request):
 def create_article(request):
     return render(request,'create_article.html')
 
+def create_full_article(request):
+    formulario=FormArticle()
+    return render(request, 'create_full_article.html' ,{
+        'form': formulario
+    })
 
 
 
