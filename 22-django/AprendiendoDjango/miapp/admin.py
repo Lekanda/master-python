f9d80a4a95efcb1cca42eaa8f40e7468c9dfc2f9
lekanda <abernaolao@gmail.com>
2020-12-13 21:45:25 +0100

    239-Primeros pasos con el Panel de Administracion

--- 22-django/AprendiendoDjango/miapp/admin.py
+++ 22-django/AprendiendoDjango/miapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Article, Category
 
 # Register your models here.
+# Al registrarlos crea un CRUD de los datos en el Admin Panel
+admin.site.register(Article)
+admin.site.register(Category)
