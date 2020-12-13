932e038aec2640c2be150f597a8f052fdb1fc8ad
lekanda <abernaolao@gmail.com>
2020-12-13 15:00:52 +0100

    236.1-Personalizar formularios .Modo1

--- 22-django/AprendiendoDjango/miapp/forms.py
+++ 22-django/AprendiendoDjango/miapp/forms.py
@@ -2,12 +2,37 @@ from django import forms
 
 class FormArticle(forms.Form):
     title = forms.CharField(
-        label="Titulo"
+        # required=False => Nos desconecta el campo 'title'. True=conectado
+        # max_length => Max numero de caracteres. "No deja meter mas"
+        label="Titulo",
+        max_length=40,
+        required=True,
+        widget=forms.TextInput(
+            attrs={
+                'placeholder':'Introduce Titulo del Articulo',
+                'class':'titulo_form_article'
+
+            }
+        )
     )
     content = forms.CharField(
         label="Contenido",
-        widget=forms.Textarea
+        max_length=250,
+        required=True,
+        widget=forms.Textarea(
+            attrs={
+                'placeholder':'Introduce Contenido del Articulo',
+                'class':'contenido_form_article'
+            }
+        )
     )
+    
+
+
+
+
+
+
     public_options=[
         (1, 'Si'),
         (0, 'No')
