a7ebe6d912b4ea434f4122fdfba9563a7dd9c5ba
lekanda <abernaolao@gmail.com>
2020-12-14 21:29:29 +0100

    240-Clase Meta en los modelos de Django 3

--- 22-django/AprendiendoDjango/miapp/models.py
+++ 22-django/AprendiendoDjango/miapp/models.py
@@ -10,7 +10,22 @@ class Article(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        # Nombre singular
+        verbose_name = "Articulo"
+        # Nombre plural
+        verbose_name_plural = "Articulos"
+        # Orden x 'propiedad'
+        ordering = ['public']
+
+
+
 class Category(models.Model):
     name = models.CharField(max_length=110)
     description= models.CharField(max_length=250)
     created_at= models.DateField()
+
+    class Meta:
+        verbose_name = "Categoria"
+        verbose_name_plural = "Categorias"
+
