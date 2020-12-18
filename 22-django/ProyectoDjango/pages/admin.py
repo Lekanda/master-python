a60e6ad1c67ba864be9f18426023b578c451d5bb
lekanda <abernaolao@gmail.com>
2020-12-18 18:26:15 +0100

    258-Configurar el panel  de Administracion de Django

--- 22-django/ProyectoDjango/pages/admin.py
+++ 22-django/ProyectoDjango/pages/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Page
 
 # Register your models here.
+admin.site.register(Page)
+
+
+############ Configuracion del Admin Panel ############
+title="Proyecto 2 Django"
+subtitle="Panel de gestion"
+# Cabecera del AdPan
+admin.site.site_header = title
+# Pestaña del AdPan
+admin.site.site_title = title
+# Subtitulo dela Adpan
+admin.site.index_title = subtitle
+
