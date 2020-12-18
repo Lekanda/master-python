b3a3b5126fd854fe582a8a6b22b8f542079f891d
lekanda <abernaolao@gmail.com>
2020-12-18 17:36:11 +0100

    256-Modelo para la entidad de Pagina.

--- 22-django/ProyectoDjango/pages/models.py
+++ 22-django/ProyectoDjango/pages/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Page(models.Model):
+   
+    title = models.CharField(max_length=50, verbose_name="Titulo")
+    content = models.TextField(verbose_name="Contenido")
+    slug = models.CharField(unique=True,max_length=150, verbose_name="URL")
+    visible = models.BooleanField(verbose_name="Visible")
+    created_at = models.models.DateTimeField(auto_now_add=True, verbose_name="Creado el ")
+    updated_at = models.models.DateTimeField(auto_now=True, verbose_name="Actualizado el ")
+
+    class Meta:
+       verbose_name = "Pagina"
+       verbose_name_plural = "Paginas"
+
+
+    def __str__(self):
+        """
+        - Funcion para personalizar los nombres de las propiedades en el Admin panel
+        """
+        return self.title
