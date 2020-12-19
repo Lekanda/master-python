91d2b3e9644c693c87ea4669afbda935fbcaaa34
lekanda <abernaolao@gmail.com>
2020-12-19 21:00:10 +0100

    275-La potencia de las relaciones.

--- 22-django/ProyectoDjango/blog/views.py
+++ 22-django/ProyectoDjango/blog/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
+# Importar los modelos
+from blog.models import Category, Article
 
 # Create your views here.
 
 def list(request):
 
+    articles=Article.objects.all()
+
     return render(request, 'articles/list.html',{
-        'title':'Articulos'
+        'title':'Articulos',
+        'articles':articles
     })
