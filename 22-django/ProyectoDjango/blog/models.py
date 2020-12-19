b48b3fd6d4a56e7e9f782743acc7cd04ba5cf356
lekanda <abernaolao@gmail.com>
2020-12-19 16:36:53 +0100

    269-Crear Modelo de categoria.

--- 22-django/ProyectoDjango/blog/models.py
+++ 22-django/ProyectoDjango/blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name=models.CharField(max_length=100, verbose_name='Nombre')
+    description=models.CharField(max_length=255,verbose_name='Descripcion')
+    create_at=models.DateTimeField(auto_now_add=True, verbose_name='Creado el')
+
+    class Meta:
+        verbose_name='Categoria'
+        verbose_name_plural='Categorias'
+    # Metodo Magico
+    def __str__(self):
+        # al crear la categoria imprime el return(nombre de categoria)
+        return self.name
