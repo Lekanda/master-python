93c2142ebc3514c7c20d9735e4dc8ef68bba4c9a
lekanda <abernaolao@gmail.com>
2020-12-21 09:59:40 +0100

    289-Pagina de Registro

--- 22-django/ProyectoDjango/mainapp/views.py
+++ 22-django/ProyectoDjango/mainapp/views.py
@@ -13,3 +13,8 @@ def about(request):
     return render(request, 'mainapp/about.html', {
         'title':'Sobre Nosotros'
     })
+
+def register_page(request):
+    return render(request, 'users/register.html',{
+        'title':'Registro'
+    })
