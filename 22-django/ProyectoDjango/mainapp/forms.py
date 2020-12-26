01dacb934856f7481fba9bad35a2b49efa803534
lekanda <abernaolao@gmail.com>
2020-12-26 10:52:16 +0100

    291-Formulario de Registro Personalizado.

---
+++ 22-django/ProyectoDjango/mainapp/forms.py
@@ -0,0 +1,12 @@
+# Importar Formularios
+from django import forms
+# Importar paquete para validar los campos
+from django.core import validators
+
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class RegisterForm(UserCreationForm):
+    class Meta:
+        model=User
+        fields=['username','email','first_name','last_name','password1','password2']
